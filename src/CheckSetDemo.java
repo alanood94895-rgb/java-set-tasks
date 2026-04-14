16e2c5877cacd31e47cb0c4fceea8428dc57cfe2
CompanyProfile <alanood94895@gmail.com>
2026-04-14 09:09:51 +0400

    task3:CheckSetDemo

--- src/CheckSetDemo.java
+++ src/CheckSetDemo.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CheckSetDemo {
+    public static void main (String[] args){
+
+        // Create a Set using HashSet implementation
+        Set <String> employeeIDs= new HashSet<>();
+
+        // Add at least 5 employee IDs
+        employeeIDs.add ("a111");
+        employeeIDs.add ("b222");
+        employeeIDs.add ("c333");
+        employeeIDs.add ("d444");
+        employeeIDs.add ("e555");
+
+        // Display all elements in the set
+        System.out.println(employeeIDs);
+
+        //Check if an exisiting ID is present
+        System.out.println(employeeIDs.contains("a111"));
+
+        //Check if non exisiting ID
+        System.out.println(employeeIDs.contains("h111"));
+
+
+
+
+
+
+    }
 }
