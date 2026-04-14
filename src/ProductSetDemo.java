06ccebb7b992f287c199031033f65279d6f35433
CompanyProfile <alanood94895@gmail.com>
2026-04-14 08:49:11 +0400

    Task 1: ProductSetDemo

---
+++ src/ProductSetDemo.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ProductSetDemo {
+
+    public static void main(String[] args) {
+
+        Set<String> productSet = new HashSet<>();
+
+        productSet.add("aaaa");
+        productSet.add("bbbb");
+        productSet.add("cccc");
+        productSet.add("eeee");
+        productSet.add("ffff");
+
+        System.out.println(productSet);
+
+        productSet.add("hhhh");
+
+        System.out.println(productSet);
+    }
+}
