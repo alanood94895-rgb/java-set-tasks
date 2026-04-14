32240f47f7b88af3281d43f5a126ab632ee2c44e
CompanyProfile <alanood94895@gmail.com>
2026-04-14 08:55:32 +0400

    Task 2: CustomerSetDemo

---
+++ src/CustomerSetDemo.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CustomerSetDemo {
+    public static void main(String[] args) {
+
+        Set<String> customerIDs = new HashSet<>();
+
+        customerIDs.add("111");
+        customerIDs.add("222");
+        customerIDs.add("333");
+        customerIDs.add("444");
+        customerIDs.add("555");
+
+        System.out.println(customerIDs);
+
+        customerIDs.add("666");
+
+        System.out.println(customerIDs);
+    }
+}
